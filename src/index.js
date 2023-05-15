3e6cf3709a173a75a387a3ab7229384f174000df
Patryk <sucheckipatryk@gmail.com>
2023-05-15 15:05:36 -0400

    Tweaks to db.json, Moved constructors next to one another, Set up a function to render Driver Images

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ const init = () => {
 
     fetch(driversURL)
         .then((resp) => resp.json())
-        .then((drivers) => console.log(drivers))
+        .then((drivers) => {
+            drivers.forEach(driver => renderDrivers(driver))
+        })
 }
 
 
 document.addEventListener("DOMContentLoaded", init);
+
+
+//Add the Driver Images to the character-bar
+
+function renderDrivers(driver) {
+    const driverImg = document.createElement('img')
+    driverImg.src = driver.image
+
+    document.querySelector('#character-bar').appendChild(driverImg)
+}

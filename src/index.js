47c344262935a331381ba5274b015f31c534f34b
SKeoughCode <131558651+SKeoughCode@users.noreply.github.com>
2023-05-15 15:25:01 -0400

    Updates for Driver Details
    Can now view drivers in the span bar as images, click on them, and view details about each driver

--- src/index.js
+++ src/index.js
@@ -6,7 +6,34 @@ const init = () => {
 
     fetch(driversURL)
         .then((resp) => resp.json())
-        .then((drivers) => console.log(drivers))
+        .then((drivers) => {
+            drivers.forEach((driver) => {
+                renderDriver(driver);
+            })
+        })
+}
+
+function renderDriver(driver) {
+
+    const driverBar = document.querySelector('#driver-bar');
+
+    let newDriverImage = document.createElement('img');
+    newDriverImage.src = driver.image;
+
+    driverBar.appendChild(newDriverImage);
+    newDriverImage.addEventListener('click', () => {
+        const detailedName = document.querySelector('#name');
+        detailedName.textContent = driver.name;
+
+        const detailedImage = document.querySelector('#image');
+        detailedImage.src = driver.image;
+
+        const pointCount = document.querySelector('#point-count');
+        pointCount.textContent = `${driver.points}`;
+
+
+    })
+
 }
 
 
